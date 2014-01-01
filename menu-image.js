4118fc2495596ebdc07e28c7075aa7256c479916
Sam Gaus <git@gaus.ie>
2013-12-31 20:21:54 -0800

    Removed console log and made the click work without reloading

--- menu-image.js
+++ menu-image.js
@@ -1,11 +1,11 @@
 jQuery(document).ready(function($){
     var _orig_send_attachment = wp.media.editor.send.attachment;
 
-    $('.menu-image-picker-button').click(function(e) {
+    $('#menu-to-edit').on('click', '.menu-image-picker-button', function(e) {
+
         var button = $(this);
         var id = button.data('item-id');
         wp.media.editor.send.attachment = function(props, attachment){
-            console.log(attachment);
             $("#selected-menu-item-image-id-"+id).val(attachment.id);
             url=(attachment.sizes.thumbnail !== undefined) ? attachment.sizes.thumbnail.url : attachment.url;
             $("#menu-image-preview-"+id).attr('src',url);
